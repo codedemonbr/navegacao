099ee3d9e77f2ab84eec272689d9f17e4478279b
Henrique <henrique.thsantos.ths@gmail.com>
2021-04-26 16:01:17 -0300

    Navegação Stack #4

--- src/components/PassoStack.js
+++ src/components/PassoStack.js
@@ -4,7 +4,22 @@ import { View, Text, Button } from "react-native";
 export default (props) => {
     return (
         <View style={{ flex: 1 }}>
-            <View>
+            <View
+                style={{
+                    flexDirection: "row",
+                    justifyContent: "space-around",
+                }}
+            >
+                {props.voltar ? (
+                    <Button
+                        title="Voltar"
+                        onPress={() => {
+                            props.navigation.goBack();
+                        }}
+                    />
+                ) : (
+                    false
+                )}
                 {props.avancar ? (
                     <Button
                         title="Avançar"
