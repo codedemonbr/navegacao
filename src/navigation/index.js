bcab85dbfa8380ee1a85733bb9a0bbbc35eda630
Henrique <henrique.thsantos.ths@gmail.com>
2021-04-26 19:14:40 -0300

    Navegação por Tabs

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { SafeAreaView, StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
-import Stack from "./Stack";
+import Tab from "./Tab";
 
 export default (props) => (
     <SafeAreaView style={{ flex: 1 }}>
@@ -14,7 +14,7 @@ export default (props) => (
             networkActivityIndicatorVisible={true}
         />
         <NavigationContainer>
-            <Stack />
+            <Tab  />
         </NavigationContainer>
     </SafeAreaView>
 );
--- src/views/TelaC.js
+++ src/views/TelaC.js
@@ -2,11 +2,8 @@ import React from "react";
 import TextoCentral from "../components/TextoCentral";
 
 export default (props) => {
-    const route = props.route || { params: { numero: 0 } };
+    const r = props.route;
+    const numero = r && r.params && r.params.numero ? r.params.numero : 0;
 
-    return (
-        <TextoCentral corFundo="#9932cd">
-            Tela C - {route.params.numero}
-        </TextoCentral>
-    );
+    return <TextoCentral corFundo="#9932cd">Tela C - {numero}</TextoCentral>;
 };
